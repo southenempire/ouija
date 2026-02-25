2f5eb6a9ab424d4b2c756791de90616323c249a3
southen_ <alabanaomi2020@icloud.com>
2026-02-25 18:19:17 +0100

    fix(core): ensure tapestry devnet url is not stripped in production

--- src/utils/socialfi.ts
+++ src/utils/socialfi.ts
@@ -2,8 +2,7 @@ import { SocialFi } from 'socialfi'
 
 const TAPESTRY_API_KEY = process.env.TAPESTRY_API_KEY
 
-const TAPESTRY_URL =
-  process.env.NODE_ENV === 'production' ? undefined : process.env.TAPESTRY_URL
+const TAPESTRY_URL = process.env.TAPESTRY_URL || 'https://api.usetapestry.dev/v1/'
 
 if (!TAPESTRY_API_KEY) {
   throw new Error('TAPESTRY_API_KEY is not set')
